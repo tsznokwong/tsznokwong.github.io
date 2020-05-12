c8af2746cee25cac41559b0537e172a8a3fe8233
tnwong <wong.tsznok@gmail.com>
2020-05-12 11:11:36 +0800

    Updates

---
+++ precache-manifest.9a0874ed8b8afe19295335331a191ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c40a76ab027fe61bfc04920b2b7de0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6b8c8a3c8c907ae5b10",
+    "url": "/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "1a81ce1a46064eec8476",
+    "url": "/static/js/2.d02bb997.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d02bb997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b8c8a3c8c907ae5b10",
+    "url": "/static/js/main.1af1950f.chunk.js"
+  },
+  {
+    "revision": "df7e48045177024f6f8d",
+    "url": "/static/js/runtime-main.1171c9e0.js"
+  }
+]);
