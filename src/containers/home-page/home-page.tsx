0ac6f7fab7b6a33ab18ff8004e886f27656a5384
tnwong <wong.tsznok@gmail.com>
2020-05-16 21:51:37 +0800

    feat: add react route

---
+++ src/containers/home-page/home-page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./home-page.css";
+
+type HomePageProps = {};
+
+const HomePage = (props: HomePageProps) => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
---
+++ src/containers/test-page/test-page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./test-page.css";
+
+type TestPageProps = {};
+
+const TestPage = (props: TestPageProps) => {
+  return <div>TestPage</div>;
+};
+
+export default TestPage;
