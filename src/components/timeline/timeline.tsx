22f02c0632127819106a1a5596935a464641b1f2
tnwong <wong.tsznok@gmail.com>
2020-08-11 17:03:04 +0800

    feat: filter timeline

--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles, Theme } from "@material-ui/core";
-import { Timeline as MUITimeline } from "@material-ui/lab";
+import {
+  Timeline as MUITimeline,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "@material-ui/lab";
 
 import PageContainer from "../page-container";
 import TimelineItem, { TimelineItemProps } from "./timeline-item";
+import { TimelineItemTypes } from "../../types/timeline-item-type";
+import TimelineItemIcon from "./timeline-item/timeline-item-icon";
 
 type TimelineProps = {
   items: TimelineItemProps[];
@@ -12,12 +18,34 @@ type TimelineProps = {
 const Timeline = (props: TimelineProps) => {
   const classes = useStyles();
   const { items } = props;
+  const [filter, setFilter] = useState(TimelineItemTypes);
   return (
     <PageContainer className={classes.root}>
-      <MUITimeline>
-        {items.map((item, index) => (
-          <TimelineItem key={`timeline_item_${index}`} {...item} />
+      <ToggleButtonGroup
+        value={filter}
+        onChange={(event, newFilter) => {
+          setFilter(newFilter);
+        }}
+        aria-label="timeline filtering"
+      >
+        {TimelineItemTypes.map((type) => (
+          <ToggleButton
+            className={classes.filterButton}
+            key={type}
+            value={type}
+            aria-label={type}
+            disableRipple
+          >
+            <TimelineItemIcon type={type} color="primary" />
+          </ToggleButton>
         ))}
+      </ToggleButtonGroup>
+      <MUITimeline>
+        {items
+          .filter((item) => (item.type ? filter.includes(item.type) : true))
+          .map((item, index) => (
+            <TimelineItem key={`timeline_item_${index}`} {...item} />
+          ))}
       </MUITimeline>
     </PageContainer>
   );
@@ -32,5 +60,16 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   connector: {
     backgroundColor: theme.palette.primary.main,
+  },
+  filterButton: {
+    "&:hover": {
+      backgroundColor: `${theme.palette.secondary.main} !important`,
+      borderColor: theme.palette.primary.main,
+    },
+    "&.Mui-selected": {
+      backgroundColor: theme.palette.secondary.light,
+      borderColor: theme.palette.primary.main,
+    },
+    borderColor: theme.palette.primary.main,
   },
 }));
--- src/types/timeline-item-type/index.ts
+++ src/types/timeline-item-type/index.ts
@@ -1,9 +1,14 @@
-type TimelineItemType =
-  | "Education"
-  | "Work"
-  | "Team/Society"
-  | "Projects"
-  | "Achievements"
-  | "Milestones";
+const ConstTimelineItemTypes = [
+  "Education",
+  "Work",
+  "Team/Society",
+  "Projects",
+  "Achievements",
+  "Milestones",
+] as const;
+
+type TimelineItemType = typeof ConstTimelineItemTypes[number];
 
 export default TimelineItemType;
+
+export const TimelineItemTypes = ConstTimelineItemTypes.map((value) => value);
