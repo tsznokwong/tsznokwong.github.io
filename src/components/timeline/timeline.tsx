41929f58b53786f92cda49dad176591a0de032d6
tnwong <wong.tsznok@gmail.com>
2021-06-11 19:16:05 +0800

    feat: sticky timeline filter

--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -8,6 +8,7 @@ import {
   useMediaQuery,
 } from "@material-ui/core";
 import { Timeline as MUITimeline } from "@material-ui/lab";
+import { Sticky, StickyContainer } from "react-sticky";
 
 import PageContainer from "../page-container";
 import TimelineItem, { TimelineItemProps } from "./timeline-item";
@@ -28,67 +29,79 @@ const Timeline = (props: TimelineProps) => {
   const isLargeScreen = useMediaQuery(theme.breakpoints.up("xl"));
   const [filter, setFilter] = useState(TimelineItemTypes);
   return (
-    <PageContainer className={`${classes.root} ${className}`}>
-      <ButtonGroup
-        size={isSmallScreen ? "small" : isLargeScreen ? "large" : "medium"}
-      >
-        {TimelineItemTypes.map((type, index) => (
-          <Tooltip
-            key={`filter_button_${index}`}
-            title={type}
-            aria-label={type}
-          >
-            <Button
-              className={
-                filter.includes(type)
-                  ? classes.filterButtonSelected
-                  : classes.filterButton
-              }
-              onClick={() => {
-                if (filter.length === TimelineItemTypes.length) {
-                  setFilter([type]);
-                } else {
-                  setFilter(
-                    TimelineItemTypes.filter((filterType) => {
-                      const isFiltered = filter.includes(filterType);
-                      const clicked = type === filterType;
-                      return !isFiltered === clicked;
-                    })
-                  );
+    <StickyContainer>
+      <PageContainer className={`${classes.root} ${className}`}>
+        <Sticky topOffset={-80}>
+          {({ style }) => (
+            <div className={classes.sticky}>
+              <ButtonGroup
+                className={classes.filterButtonGroup}
+                size={
+                  isSmallScreen ? "small" : isLargeScreen ? "large" : "medium"
                 }
-              }}
-              disableRipple
-            >
-              <TimelineItemIcon type={type} color="primary" />
-            </Button>
-          </Tooltip>
-        ))}
-      </ButtonGroup>
-      <MUITimeline className={classes.timeline}>
-        {items
-          .filter((item) => {
-            if (item.category === "year-stamp") {
+                style={{ ...style, left: undefined, width: undefined }}
+              >
+                {TimelineItemTypes.map((type, index) => (
+                  <Tooltip
+                    key={`filter_button_${index}`}
+                    title={type}
+                    aria-label={type}
+                  >
+                    <Button
+                      className={
+                        filter.includes(type)
+                          ? classes.filterButtonSelected
+                          : classes.filterButton
+                      }
+                      onClick={() => {
+                        if (filter.length === TimelineItemTypes.length) {
+                          setFilter([type]);
+                        } else {
+                          setFilter(
+                            TimelineItemTypes.filter((filterType) => {
+                              const isFiltered = filter.includes(filterType);
+                              const clicked = type === filterType;
+                              return !isFiltered === clicked;
+                            })
+                          );
+                        }
+                      }}
+                      disableRipple
+                    >
+                      <TimelineItemIcon type={type} color="primary" />
+                    </Button>
+                  </Tooltip>
+                ))}
+              </ButtonGroup>
+            </div>
+          )}
+        </Sticky>
+        <MUITimeline className={classes.timeline}>
+          {items
+            .filter((item) => {
+              if (item.category === "year-stamp") {
+                return true;
+              }
+              if (item.type && TimelineItemTypes.includes(item.type)) {
+                return filter.includes(item.type);
+              }
               return true;
-            }
-            if (item.type && TimelineItemTypes.includes(item.type)) {
-              return filter.includes(item.type);
-            }
-            return true;
-          })
-          .filter((item, index, items) => {
-            if (item.category === "normal") {
-              return true;
-            }
-            return (
-              items[index - 1]?.category !== "year-stamp" ||
-              items[index + 1]?.category !== "year-stamp"
-            );
-          })
-          .map((item, index) => (
-            <TimelineItem key={`timeline_item_${index}`} {...item} />
-          ))}
-      </MUITimeline>
-    </PageContainer>
+            })
+            .filter((item, index, items) => {
+              if (item.category === "normal") {
+                return true;
+              }
+              return (
+                items[index - 1]?.category !== "year-stamp" ||
+                items[index + 1]?.category !== "year-stamp"
+              );
+            })
+            .map((item, index) => (
+              <TimelineItem key={`timeline_item_${index}`} {...item} />
+            ))}
+        </MUITimeline>
+      </PageContainer>
+    </StickyContainer>
   );
 };
 
@@ -101,6 +114,15 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   connector: {
     backgroundColor: theme.palette.primary.main,
+  },
+  sticky: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  filterButtonGroup: {
+    zIndex: 999,
   },
   filterButton: {
     borderColor: theme.palette.primary.main,
