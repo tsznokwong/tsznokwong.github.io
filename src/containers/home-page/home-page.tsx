40c29567cf7daf577d6e84ec2a8f0c930a8fd07f
tnwong <wong.tsznok@gmail.com>
2022-03-23 15:27:18 +0800

    chore: update homepage

--- src/containers/home-page/home-page.tsx
+++ src/containers/home-page/home-page.tsx
@@ -3,6 +3,7 @@ import { makeStyles, Theme, Container } from "@material-ui/core";
 
 import ParagraphSection from "../../components/paragraph-section/paragraph-section";
 import Data from "../../assets/data/home-page.json";
+import Portrait0 from "../../assets/images/portrait-0.svg";
 import Firebird from "../../assets/images/hkust-firebird.svg";
 
 type HomePageProps = {};
@@ -12,8 +13,15 @@ const HomePage = (props: HomePageProps) => {
   return (
     <Container className={classes.root} maxWidth={false} disableGutters>
       <ParagraphSection
+        className={classes.introContainer}
         title={Data.title}
         subtitle={Data.subtitle}
+        background={Portrait0}
+      />
+      <ParagraphSection
+        className={classes.sectionUniContainer}
+        title={Data.section_uni_title}
+        subtitle={Data.section_uni_subtitle}
         background={Firebird}
       />
     </Container>
@@ -28,4 +36,14 @@ const useStyles = makeStyles((theme: Theme) => ({
     display: "flex",
     flexDirection: "column",
   },
+  introContainer: {
+    marginTop: "1rem",
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    backgroundPositionX: "75%",
+    backgroundPositionY: "25%",
+    padding: "28vh 0 6vh",
+  },
+  sectionUniContainer: {
+  }
 }));
